5b4464e88e17660fdac983fe75598e56e11ffc45
AnimosAnimos <vijayakumartamalampudi@gmail.com>
2019-09-11 00:25:54 +0530

    files added

---
+++ javascript/jsFunctions/jsFunction.js
@@ -0,0 +1,17 @@
+// function syntax
+// function defination
+// function functionName(argument1,argument2)
+// {
+// 	statements;
+// 	return result;
+// }
+// function calling
+// functionName(argument1,argument2);
+
+
+function square(n)
+{
+	console.log(n*n);
+}
+
+square(3);
